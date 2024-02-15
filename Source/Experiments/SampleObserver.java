7e97fbd791498abb9052ebce0dcd232d4f934105
Manfred Jaeger <jaeger@cs.aau.dk>
2024-02-15 16:22:16 +0100

    experiments added

---
+++ Source/Experiments/SampleObserver.java
@@ -0,0 +1,18 @@
+package Experiments;
+
+import java.util.*;
+
+import RBNinference.SampleProbs;
+import RBNutilities.rbnutilities;
+
+public class SampleObserver implements Observer{
+
+	public void update(Observable o, Object arg){
+		if (o instanceof SampleProbs){
+			double [][] prob= ((SampleProbs)o).getProbs();
+				System.out.println("Number of samples: " + ((SampleProbs)o).getSize());
+				System.out.println(rbnutilities.MatrixToString(prob));
+				System.out.println();
+		}
+	}
+}
