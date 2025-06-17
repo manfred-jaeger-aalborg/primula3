affe815cb9d9ddbe638312f515f84e15d2abffa2
Raffaele Pojer <raffaelepojer@gmail.com>
2025-06-17 15:04:45 +0200

    Refactor relation names and update GNN configuration to align with revised RBN syntax. Update JAR.

--- Examples/WaterPollution/config_model.py
+++ Examples/WaterPollution/config_model.py
@@ -22,19 +22,19 @@ class HeteroGraph(nn.Module):
         for layer_idx in range(num_layers):
             if layer_idx == 0:
                 conv_dict = {
-                    ('sub', 'to', 'sub'): SAGEConv(in_sub, hidden_dims, aggr=sage_aggr),
-                    ('hru_agr', 'to', 'sub'): SAGEConv((in_hru_agr, in_sub), hidden_dims, aggr=sage_aggr),
-                    ('sub', 'to', 'hru_agr'): SAGEConv((in_sub, in_hru_agr), hidden_dims),
-                    ('hru_urb', 'to', 'sub'): SAGEConv((in_hru_urb, in_sub), hidden_dims, aggr=sage_aggr),
-                    ('sub', 'to', 'hru_urb'): SAGEConv((in_sub, in_hru_urb), hidden_dims)
+                    ('sub', 'downstream', 'sub'): SAGEConv(in_sub, hidden_dims, aggr=sage_aggr),
+                    ('agr', 'downstream_agr', 'sub'): SAGEConv((in_hru_agr, in_sub), hidden_dims, aggr=sage_aggr),
+                    ('sub', 'upstream_agr', 'agr'): SAGEConv((in_sub, in_hru_agr), hidden_dims),
+                    ('urb', 'downstream_urb', 'sub'): SAGEConv((in_hru_urb, in_sub), hidden_dims, aggr=sage_aggr),
+                    ('sub', 'upstream_urb', 'urb'): SAGEConv((in_sub, in_hru_urb), hidden_dims)
                 }
             else:
                 conv_dict = {
-                    ('sub', 'to', 'sub'): SAGEConv(self.out_dim_sub, hidden_dims),
-                    ('hru_agr', 'to', 'sub'): SAGEConv((in_hru_agr, self.out_dim_sub), hidden_dims, aggr=sage_aggr),
-                    ('sub', 'to', 'hru_agr'): SAGEConv((self.out_dim_sub, in_hru_agr), hidden_dims),
-                    ('hru_urb', 'to', 'sub'): SAGEConv((in_hru_urb, self.out_dim_sub), hidden_dims, aggr=sage_aggr),
-                    ('sub', 'to', 'hru_urb'): SAGEConv((self.out_dim_sub, in_hru_urb), hidden_dims)
+                    ('sub', 'downstream', 'sub'): SAGEConv(self.out_dim_sub, hidden_dims),
+                    ('agr', 'downstream_agr', 'sub'): SAGEConv((in_hru_agr, self.out_dim_sub), hidden_dims, aggr=sage_aggr),
+                    ('sub', 'upstream_agr', 'agr'): SAGEConv((self.out_dim_sub, in_hru_agr), hidden_dims),
+                    ('urb', 'downstream_urb', 'sub'): SAGEConv((in_hru_urb, self.out_dim_sub), hidden_dims, aggr=sage_aggr),
+                    ('sub', 'upstream_urb', 'urb'): SAGEConv((self.out_dim_sub, in_hru_urb), hidden_dims)
                 }
 
             self.layers.append(HeteroConv(conv_dict, aggr=self.heteroAggr))
@@ -46,8 +46,8 @@ class HeteroGraph(nn.Module):
         self.final_lin = nn.Linear(self.out_dim_sub, out_dims)
 
     def forward(self, x_dict: dict, edge_index_dict: dict) -> torch.Tensor:
-        skip_hru_agr = x_dict['hru_agr']
-        skip_hru_urb = x_dict['hru_urb']
+        skip_hru_agr = x_dict['agr']
+        skip_hru_urb = x_dict['urb']
         for i, layer in enumerate(self.layers):
             x_dict = layer(x_dict, edge_index_dict)
 
@@ -55,8 +55,8 @@ class HeteroGraph(nn.Module):
             x_dict['sub'] = self.dropout(x_dict['sub'])
 
             # Reapply the original features for non-'sub' nodes (as skip conn)
-            x_dict['hru_agr'] = skip_hru_agr
-            x_dict['hru_urb'] = skip_hru_urb
+            x_dict['agr'] = skip_hru_agr
+            x_dict['urb'] = skip_hru_urb
 
         out = self.final_lin(x_dict['sub'])
         return F.softmax(out, dim=1)
@@ -64,10 +64,9 @@ class HeteroGraph(nn.Module):
 def load_model():
     model = HeteroGraph(in_sub= 2, in_hru_agr=5, in_hru_urb=10, hidden_dims=20, out_dims=3, num_layers=2, batch_norm=True).to("cpu")
     model.load_state_dict(torch.load(
-        '/Users/lz50rg/Dev/primula-workspace/primula3/Examples/WaterPollution/model_l21e3_bn.pt',
+        '/Users/lz50rg/Dev/primula-workspace/primula3/Examples/WaterPollution/model_l21e5_bn.pt',
         map_location="cpu",
         weights_only=True
     ))
     model.eval()
     return model, 2
-
